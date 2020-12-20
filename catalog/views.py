ee4b22adac7ad88f4ba026e3f8f777713ac0d830
Mirza Golam Abbas Shahneel <mgashahneel@gmail.com>
2020-12-20 17:45:09 +0600

    generic class-based list and detail views

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
 from .models import Book, Author, BookInstance, Genre
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+
 
 def index(request):
     """View function for home page of site."""
@@ -15,12 +31,24 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    # To generate genre counts
+    num_genres = Genre.objects.count()
+
     context = {
         'num_books' : num_books,
         'num_instances' : num_instances,
         'num_instances_available' : num_instances_available,
         'num_authors' : num_authors,
+        'num_genres' : num_genres,
     }
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+def book_detail_view(request, primary_key):
+    try:
+        book = Book.objects.get(pk=primary_key)
+    except Book.DoesNotExist:
+        raise Http404('Book does not exist')
+
+    return render(request, 'catalog/book_detail.html', context={'book': book})
